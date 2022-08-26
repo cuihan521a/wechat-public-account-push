c60f3310cc20d58d1b66e22c5351b9cc8c0f9c0b
834880612 <96238441+834880612@users.noreply.github.com>
2022-08-27 07:59:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,17 +7,17 @@ export const config = {
     // 模板消息id
     templateId: "q-fId2TfXUOXdeo712uQYYaKY6SfreMCN6dR1MAhHjM",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oRgft6ncRFn5Ab24EAEeIQTP2-J4"],
+    user: ["oRgft6pIQf1Y1S8cyMlZex-mGRBA"],
      
     // 信息配置
     // 所在省份
     province: "河南",
     // 所在城市
-    city: "驻马店",
+    city: "郑州",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "张奕", "year": "2000", "date": "11-11", "type": 'new'},
-      {"name": "张奕妹妹", "year": "2000", "date": "08-09", "type": 'new'},
+      {"name": "景小美", "year": "2000", "date": "11-11", "type": 'new'},
+      {"name": "000", "year": "2000", "date": "08-09", "type": 'new'},
     ],
     // 在一起的日子，格式同上
     loveDate: "2022-05-01",
